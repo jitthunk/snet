b7970eccfeee31059e152544f1a6cdfd0110604a
JT <37082202+jitthunk@users.noreply.github.com>
2022-10-24 18:08:02 +0530

    basic server

--- server.go
+++ server.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi from the server %s", r.URL.Path[1:])
+}
 func main() {
-	fmt.Print("Hello from server")
+	fmt.Print("Server starting")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
